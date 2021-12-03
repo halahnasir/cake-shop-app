0cedfdf0d46e817305843cc4ae20d37aec907e59
halahnasir <hnasir77@gmail.com>
2021-12-03 15:25:44 +0500

    Connecting React App To Redux

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import buyCake from '../redux/cakes/CakeActions'
+import {connect} from 'react-redux';
 
-const CakeContainer = () => {
+//Define these function
+const mapStateToProps = (state) => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+//Component 
+
+const CakeContainer = (props) => {
     return(
         <div>
-            <h2>Number of Cakes</h2>
-            <button>Buy Cake</button>
+            <h2>Number of Cakes: {props.numOfCakes}</h2>
+            <button onClick = {props.buyCake}>Buy Cake</button>
         </div>
     )
 }
 
-export default CakeContainer;
+//Connect these functions with the component
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
