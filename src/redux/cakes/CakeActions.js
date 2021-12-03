99f890d4ac71776f6ff26cd4d1bac8a8c7ba82ae
halahnasir <hnasir77@gmail.com>
2021-12-03 14:09:13 +0500

    Action & Action Creator

---
+++ src/redux/cakes/CakeActions.js
@@ -0,0 +1,12 @@
+//All the Action Creators for the app will be defined here.
+
+//import the action
+import {BUY_CAKE} from './CakeType';
+
+const buyCake = () => {
+    return{
+        type: BUY_CAKE
+    }
+}
+
+export default buyCake;
