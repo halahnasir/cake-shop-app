f7e032d4ee66cbdb0a8e956f5a5d5fcd019c3cf1
halahnasir <hnasir77@gmail.com>
2021-12-03 14:19:51 +0500

    Reducer

--- src/redux/cakes/CakeReducer.js
+++ src/redux/cakes/CakeReducer.js
@@ -1,7 +1,25 @@
 // Reducers will be defined here
 
+//Importing action type
+import {BUY_CAKE} from './CakeType';
 
 // We start off with 10 cakes in the shop/inventory
 const initialState = {
     numOfCakes: 10
 }
+
+//Reducer
+
+const cakeReducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE: return{
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state
+    }
+}
+
+//To be able to use this reducer in a different file, we need to export it
+
+export default cakeReducer;
