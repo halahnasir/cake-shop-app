21a44030077798cf8996238b4e694417764f2c17
halahnasir <hnasir77@gmail.com>
2021-12-03 13:57:08 +0500

    Initial Setup

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const CakeContainer = () => {
     return(
         <div>
-
+            <h2>Number of Cakes</h2>
+            <button>Buy Cake</button>
         </div>
     )
 }
