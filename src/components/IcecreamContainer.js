0822a045721bb045be39b15f1a300e7d60303e1f
halahnasir <hnasir77@gmail.com>
2021-12-06 18:16:25 +0500

    Icecream Container

---
+++ src/components/IcecreamContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IcecreamContainer = () => {
+    return (
+        <div>
+            <h2>Number of Icecreams: </h2>
+            <button>Buy Icecream</button>
+        </div>
+    )
+}
+
+export default IcecreamContainer
