78fe11b857f249715fdf9ba6116bc2bc433311b3
halahnasir <hnasir77@gmail.com>
2021-12-06 17:54:03 +0500

    Comining Multiple Reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import cakeReducer from './cakes/CakeReducer';
+import icecreamReducer from './icecream/IcecreamReducer';
+
+//Comibing Reducers
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    icecCrem: icecreamReducer
+})
 
 const store = createStore(cakeReducer);
 
